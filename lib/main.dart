9f11ba79b369d744a1953e6522c5fbeb275ad20e
Victor <johndoe@example.com>
2024-10-10 22:48:20 -0300

    início interface

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,36 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return MaterialApp(
+      title: "SQLite CRUD",
+      home: Pagina1(),
+    );
   }
+}
+class Pagina1 extends StatefulWidget {
+  const Pagina1({super.key});
 
+  @override
+  State<Pagina1> createState() => _Pagina1State();
+}
+
+class _Pagina1State extends State<Pagina1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SQLite CRUD'),
+      ),
+      body: const Column(
+        children: [
+          Padding(padding: EdgeInsets.all(10.0),
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'Nome'
+            ),
+          )),
+        ],
+      ),
+    );
+  }
 }
